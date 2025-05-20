ee7d969473712955328e2a502955f6e86ea76cb4
ludmiladichiera <ludmiladichiera@gmail.com>
2025-05-20 17:13:23 -0300

    agregue proyecto negocio y dominio

--- TpAPIs_equipo-10A/App_Start/WebApiConfig.cs
+++ TpAPIs_equipo-10A/App_Start/WebApiConfig.cs
@@ -19,6 +19,7 @@ namespace TpAPIs_equipo_10A
                 routeTemplate: "api/{controller}/{id}",
                 defaults: new { id = RouteParameter.Optional }
             );
+            config.Formatters.Remove(config.Formatters.XmlFormatter);
         }
     }
 }
