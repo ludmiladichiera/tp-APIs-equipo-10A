6fc2acc6b63877c24572d35a44a75302fa64ed20
gerstnerLeandro <leandrogerstner00@gmail.com>
2025-05-20 18:25:03 -0300

    agregado el desarrollo de  Articulos y Categoria en dominio

--- Dominio/Articulo.cs
+++ Dominio/Articulo.cs
@@ -8,5 +8,15 @@ namespace Dominio
 {
     internal class Articulo
     {
+        public int Id { get; set; }
+        public string Codigo { get; set; }
+        public string Nombre { get; set; }
+        public string Descripcion { get; set; }
+        public Marca Marca { get; set; }
+        public Categoria Categoria { get; set; }
+        public List<Imagen> Imagenes { get; set; }
+        public decimal? Precio { get; set; }
+
     }
 }
+
--- Dominio/Categoria.cs
+++ Dominio/Categoria.cs
@@ -8,5 +8,21 @@ namespace Dominio
 {
     internal class Categoria
     {
+        public int Id { get; set; }
+        public string Descripcion { get; set; }
+
+        public Categoria() { }
+        public Categoria(int id, string descripcion)
+        {
+            Id = id;
+            Descripcion = descripcion;
+        }
+
+        public override string ToString()
+        {
+            return Descripcion;
+        }
+
     }
+}
 }
